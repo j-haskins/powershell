efd7f76ea37ff8ed8959090f7392d25c26b00f30
Jay Haskins <43075457+j-haskins@users.noreply.github.com>
2026-09-23 15:00:07 -0700

    Update winget.ps1 to include more applications
    Reorganize and add additional applications to installation script.

--- winget.ps1
+++ winget.ps1
@@ -1,53 +1,46 @@
-# Notepad++
-winget install -e -h --id Notepad++.Notepad++
-
-# VS Code
-winget install -e -h --id Microsoft.VisualStudioCode
-
 # PowerShell Core
 winget install -e -h --id Microsoft.PowerShell
 
 # Window Terminal
 winget install -e -h --id Microsoft.WindowsTerminal
 
-# Azure Cli
-winget install -e -h --id Microsoft.AzureCLI
-
-# Firefox 
-winget install -e -h --id Mozilla.Firefox
-
 # Git
 winget install -e -h --id Git.Git
 winget install GitHub.Copilot
 
+# Thunderbird
+winget install -e --id Mozilla.Thunderbird
+
+#Spotify
+winget install -e --id Spotify.Spotify
+
+# Additional Apps
+# Notepad++
+winget install -e -h --id Notepad++.Notepad++
+# VS Code
+winget install -e -h --id Microsoft.VisualStudioCode
+# Azure Cli
+winget install -e -h --id Microsoft.AzureCLI
+# Firefox 
+winget install -e -h --id Mozilla.Firefox
 # Podman
 winget install -e -h --id RedHat.Podman-Desktop
 winget install -e -h --id RedHat.Podman
-
 # Burp Suite Community
 winget install -e -h --id PortSwigger.BurpSuite.Community
-
-# # Terraform
+# Terraform
 winget install -e -h --id Hashicorp.Terraform
-
-# # dotnet 8
-# winget install -e -h --id Microsoft.DotNet.SDK.8
-
-# # NuGet
+# dotnet 8
+winget install -e -h --id Microsoft.DotNet.SDK.8
+# NuGet
 winget install -e -h --id Microsoft.NuGet
-
 # # Node.js
 winget install -e -h --id OpenJS.NodeJS
-
-# # Python
+# Python
 winget install -e -h --id Python.Python.3.13
-
-# # Docker
+# Docker
 winget install -e -h --id Docker.DockerDesktop
 winget install -e -h --id Docker.DockerCLI
-
-# Thunderbird
-winget install -e --id Mozilla.Thunderbird
 
 # Update
 winget upgrade all -h
